3e683218f1f2b4bf0c8b3289ad01c726df0b95e2
ajit009kumar <ajit009kumar@gmail.com>
2018-01-08 22:03:36 +0530

    uncommented the syntax

--- playground/mongod-update.js
+++ playground/mongod-update.js
@@ -6,15 +6,15 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(error,db) => {
           return console.log('Unable to connect to db');
         }
         console.log('connected to the db');
-        // db.collection('Todos').findOneAndUpdate({
-        //     _id: new ObjectID('5a536497dc12c0c9753cf995')
-        // },{
-        //     $set: {completed: false}
-        // },{
-        //     returnOriginal: false
-        // }).then((res) => {
-        //     console.log(res);
-        // });
+        db.collection('Todos').findOneAndUpdate({
+            _id: new ObjectID('5a536497dc12c0c9753cf995')
+        },{
+            $set: {completed: false}
+        },{
+            returnOriginal: false
+        }).then((res) => {
+            console.log(res);
+        });
 
         db.collection('Users').findOneAndUpdate({
           _id: new ObjectID('5a538423fb39843b105da006')
