f26ebf83e3883ee5c8095093f8bbc0a066f3bc28
ajit009kumar <ajit009kumar@gmail.com>
2018-01-11 23:13:57 +0530

    added the functionality to test the app

--- server/server.js
+++ server/server.js
@@ -1,41 +1,76 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+
+
 var { mongoose } = require('./db/mongoose');
-var { Todo } = require('./models/todos');
+var { Todo } = require('./models/todo');
 var { User } = require('./models/users');
 
 
-
-var newTodo = new Todo({
-    text: '  Hii First Model design  '
-});
-
-newTodo.save().then((docs) => {
-   console.log(docs);
-},(error) => {
-    console.log('  Failed to save the Todo  ');
-});
+var app = express();
 
 
-var secondTodo = new Todo(
-{
-    text: 'Insert the second Record',
-    completed: true,
-    completedAt: 25
-});
+app.use(bodyParser.json());
 
-secondTodo.save().then((doc) => {
-   console.log(doc);
-},(err) => {
-   return console.log('Unable to connect');
+app.post('/todos',(req,res) => {
+
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+         res.send(doc);
+    },(e) => {
+        res.status(400).send(e);  
+    });
 });
 
 
+app.listen('3000',() => {
+    console.log('listening on 3000');
+ });
+ 
 
-var newUser = new User({
-    email: 'ajeet'
-});
 
-newUser.save().then((docs) => {
-    console.log(docs);
-},(error) => {
-    console.log('Unable to connect');
-});
+
+
+// var newTodo = new Todo({
+//     text: '  Hii First Model design  '
+// });
+
+// newTodo.save().then((docs) => {
+//    console.log(docs);
+// },(error) => {
+//     console.log('  Failed to save the Todo  ');
+// });
+
+
+// var secondTodo = new Todo(
+// {
+//     text: 'Insert the second Record',
+//     completed: true,
+//     completedAt: 25
+// });
+
+// secondTodo.save().then((doc) => {
+//    console.log(doc);
+// },(err) => {
+//    return console.log('Unable to connect');
+// });
+
+
+
+// var newUser = new User({
+//     email: 'ajeet'
+// });
+
+// newUser.save().then((docs) => {
+//     console.log(docs);
+// },(error) => {
+//     console.log('Unable to connect');
+// });
+
+
+
+module.exports = { app };
