0f8b883535b68459bd6cbfb4b20e280668eb2b2a
ajit009kumar <ajit009kumar@gmail.com>
2018-01-19 22:18:57 +0530

    Added the config and set the environment and created a new database for test purpose

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,13 +4,13 @@ var url = 'mongodb://localhost:27017/test';
 
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
-var conn = MongoClient.connect(url, function(err, db) {
-    var adminDb = db.admin();
-    adminDb.serverStatus(function(err, info) {
-        console.log(info.version);
-    })
-})
+mongoose.connect(process.env.MONGODB_URI);
+// var conn = MongoClient.connect(url, function(err, db) {
+//     var adminDb = db.admin();
+//     adminDb.serverStatus(function(err, info) {
+//         console.log(info.version);
+//     })
+// })
 
 
 module.exports = {
