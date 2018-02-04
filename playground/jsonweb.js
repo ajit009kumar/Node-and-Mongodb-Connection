c82bb96b5252c11d28206a1577c4b63b3f988338
ajit009kumar <ajit009kumar@gmail.com>
2018-02-05 00:14:13 +0530

    added the hashing function to the passord

--- playground/jsonweb.js
+++ playground/jsonweb.js
@@ -1,13 +1,31 @@
 
 const jwt = require('jsonwebtoken');
 
-let data = {
-    id: 10
-};
+const bcrypt = require('bcryptjs');
 
-let token = jwt.sign(data, '123abc');
-console.log(token);
+// let data = {
+//     id: 10
+// };
 
-let decoded = jwt.verify(token, '123abc');
-console.log('decoded',decoded);
+// let token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// let decoded = jwt.verify(token, '123abc');
+// console.log('decoded',decoded);
+
+
+let password = 'password123!';
+
+bcrypt.genSalt(10, ( err, salt ) => {
+    bcrypt.hash( password, salt , (err, hash) => {
+        console.log(hash);
+    });
+});
+
+let hashedPassword = '$2a$10$yaAu5lOJybfJMs1rz9oRk.rWT4QMq5uXSaZhFtHYT2FXkeLDoqh32';
+
+bcrypt.compare( password, hashedPassword , (err,res) => {
+        console.log(res);
+});
+
 
